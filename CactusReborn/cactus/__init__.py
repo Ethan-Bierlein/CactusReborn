303e3dea256d4f436ae90ea4bfb7431944958a1c
ShearOfDoom <ebr4d@live.com>
2015-09-17 21:36:53 -0500

    *sigh*. Let's try this again.

---
+++ CactusReborn/cactus/__init__.py
@@ -0,0 +1,8 @@
+"""
+__init__.py (./cactus)
+
+__init__ serves an an import tool, taking away the need
+to import every individual function and class in a Cactus
+project.
+"""
+from .main import *
---
+++ CactusReborn/vs_start.py
@@ -0,0 +1,9 @@
+"""
+vs-start.py
+
+This is just a small helper file, to ensure that the
+project will actually build and run in Visual Studio.
+This file will not be included in the releases. This
+is also where tests of the library will be performed.
+"""
+import cactus
