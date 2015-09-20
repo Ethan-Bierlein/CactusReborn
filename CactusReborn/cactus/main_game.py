381f2ae91e5e49daf48e9e41696666de92da4c8f
ShearOfDoom <ebr4d@live.com>
2015-09-20 14:22:05 -0500

    Added some documentation to the play_game function.

--- CactusReborn/cactus/main_game.py
+++ CactusReborn/cactus/main_game.py
@@ -9,6 +9,17 @@ import sys
 
 
 def play_game(*, name, description, prompt, flowchart, case_sensitive, error_message, global_commands):
+    """Play a user-created game.
+
+    Keyword arguments:
+    name            -- The name of the game.
+    description     -- The description of the game.
+    prompt          -- The game's prompt.
+    flowchart       -- The flowchart that the player traverses.
+    case_sensitive  -- Whether or not user input is lowered.
+    error_message   -- The message to display when the user enters invalid input.
+    global_commands -- Global commands that can be executed anywhere.
+    """
     current_location = flowchart.find_start()
     
     if current_location is not None:
