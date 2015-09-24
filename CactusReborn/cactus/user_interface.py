c7ab4bf6f632e172b29cf652086b07dac3bd7b9f
ShearOfDoom <ebr4d@live.com>
2015-09-23 20:02:32 -0500

    Create a skeleton for future user interface functionality.

--- CactusReborn/cactus/user_interface.py
+++ CactusReborn/cactus/user_interface.py
@@ -1,6 +1,16 @@
-"""
+﻿"""
 user_interface.py
 
 This file contains the basic classes and methods used
 to create a "UI/menu" of sorts.
 """
+
+
+def run_user_interface(*, title, error_message, options):
+    """Run a user interface.
+
+    This function displays an interface to a user, and
+    presents them with various options, which each *should*
+    link to a MainGame instance
+    """
+    pass
