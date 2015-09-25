9cd47c54acf6fa6ca00e702508004bca14175a20
ShearOfDoom <ebr4d@live.com>
2015-09-24 21:19:33 -0500

    Added a setup file for easy install.

---
+++ CactusReborn/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+from distutils.core import setup, find_packages
+
+setup(
+    name="Cactus",
+    packages=find_packages(),
+    version="0.1.0",
+    description="A simple Python game engine for creating text-based adventure games.",
+    author="Ethan Bierlein and Project Contributors",
+    author_email="ebr4d@live.com",
+    url="https://github.com/ShearOfDoom/CactusReborn",
+    download_url="https://github.com/ShearOfDoom/CactusReborn/archive/v0.1.0.zip",
+    keywords=[],
+    classifiers=[]
+)
